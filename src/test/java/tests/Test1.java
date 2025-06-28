bc58130fdc9738d95d800e89be005bfaa310f7c8
zkoymen <zeynepkoymen9@gmail.com>
2025-06-28 17:45:39 +0300

    Added driver file and demo test completed

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        // Identify driver
+        System.setProperty("chromeDriver", "/drivers/chromedriver");
+
+        // Create the driver
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://demoqa.com/");
+        driver.
+
+    }
+}
