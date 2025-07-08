0182ea3831c90b9c73ae9489a65387f2fc6a803b
zkoymen <zeynepkoymen9@gmail.com>
2025-07-08 23:42:38 +0300

    fixed bugs -closing gracefully after 2 sec now

--- src/test/java/tests/WindowTabTest.java
+++ src/test/java/tests/WindowTabTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import javax.xml.stream.events.StartDocument;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,37 +19,35 @@ public class WindowTabTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://demoqa.com/browser-windows");
 
+
+        // Capture main window for future operations
+        String mainHandle = driver.getWindowHandle();
+
         WebElement tabBtn = driver.findElement(By.id("tabButton"));
         tabBtn.click();
 
         WebElement windowBtn = driver.findElement(By.id("windowButton"));
         windowBtn.click();
 
-        // Window handles
+
+        // this is a set and not guaranteed to be ordered like --> main, tab, window
+        // thus --> remove main and close them in an order
         List<String> tabs = new ArrayList<>(driver.getWindowHandles());
 
-
         System.out.println(tabs.size());
-
-        // Switch to newly opened tab
-        driver.switchTo().window(tabs.get(1));
-
-
-        //System.out.println((tabs.stream().toList()));
-
-
-        System.out.println(driver.getCurrentUrl());
-
-        // Wait a bit before closing the tab
-        Thread.sleep(Duration.ofSeconds(2));
-
-        // Close the tab or window --> not the driver
-        // quit deletes the driver
-        // !! --> it closes the latest opened one
-        driver.close();
+        tabs.remove(mainHandle);
 
 
 
+        for(String handle : tabs) {
+            driver.switchTo().window(handle);
+            System.out.println(driver.getCurrentUrl());
+            Thread.sleep(Duration.ofSeconds(2));        // or use(2_000) as 2 seconds
+            driver.close();
+        }
+
+        // Get to main handle
+        driver.switchTo().window(mainHandle);
 
     }
 }
